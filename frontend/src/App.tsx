0736662f5659d1483fb94becc892d929f278990b
Wilber Arruda <warruda01@gmail.com>
2022-11-19 23:09:38 +0000

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
-  return(
+  return (
+    <>
 <h1>Ola mundo!</h1>
+<NotificationButton/>
+<NotificationButton/>
+</>
   )
 }
 
